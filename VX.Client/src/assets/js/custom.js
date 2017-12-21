97ca0017dcc5e0aa693bfadde9265f55fea56e9a
thanhdai18ht <thanhdai18ht@gmail.com>
2017-12-22 01:14:24 +0700

    menu

--- VX.Client/src/assets/js/custom.js
+++ VX.Client/src/assets/js/custom.js
@@ -6,20 +6,20 @@ $(document).ready(function() {
   });
  });
  
- $("#video").simplePlayer();
- jQuery(document).ready(function($) {
-  $('.counter').counterUp({
-   delay: 100,
-   time: 1000
-  });
- });
+ //$("#video").simplePlayer();
+//  jQuery(document).ready(function($) {
+//   $('.counter').counterUp({
+//    delay: 100,
+//    time: 1000
+//   });
+//  });
  $(function() {});
- $(window).load(function() {
-  $('.flexslider').flexslider({
-   animation: "slide",
-   start: function(slider) {
-    $('body').removeClass('loading');
-   }
-  });
- });
+//  $(window).load(function() {
+//   $('.flexslider').flexslider({
+//    animation: "slide",
+//    start: function(slider) {
+//     $('body').removeClass('loading');
+//    }
+//   });
+//  });
 });
