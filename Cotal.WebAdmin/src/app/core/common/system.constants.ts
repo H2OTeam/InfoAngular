7f8cc2a5057b6930af76eb2a125ef9b385c8547c
xuanphap <phapht@gmail.com>
2017-12-24 12:23:44 +0700

    fixed user managament

--- Cotal.WebAdmin/src/app/core/common/system.constants.ts
+++ Cotal.WebAdmin/src/app/core/common/system.constants.ts
@@ -1,4 +1,5 @@
 ﻿export class SystemConstants {
     public static CURRENT_USER = "currentUser";
-    public static BASE_API = "http://core.cotal.com.vn/api/v1/"; 
+     public static BASE_API = "http://core.cotal.com.vn/api/v1/"; 
+   // public static BASE_API = "http://localhost:8080/api/v1/"; 
 }
