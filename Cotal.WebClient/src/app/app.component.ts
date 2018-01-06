3ab56393bfa190b92071465359db86b354be0bd2
xuanphap <phapht@gmail.com>
2018-01-06 17:39:06 +0700

    Fixed logo

--- Cotal.WebClient/src/app/app.component.ts
+++ Cotal.WebClient/src/app/app.component.ts
@@ -12,37 +12,37 @@ export class AppComponent implements AfterViewChecked {
   title = 'app';
   ngAfterViewChecked() {
 
-    // var s = document.createElement("script");
-    // s.type = "text/javascript";
-    // s.src = "../assets/js/theme.js";
-    // this.elementRef.nativeElement.appendChild(s);
-    // let owl = $('#owl-main-slider');
-    // owl.owlCarousel({
-    //   items: 1,
-    //   loop: true,
-    //   margin: 10,
-    //   autoplay: true,
-    //   autoplayTimeout: 3000,
-    //   autoplayHoverPause: true,
-    // });
-    // var owl2 = $('#partners');
-    // owl2.owlCarousel({
-    //   items: 4,
-    //   loop: true,
-    //   margin: 10,
-    //   autoplay: true,
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "../assets/js/theme.js";
+    this.elementRef.nativeElement.appendChild(s);
+    let owl = $('#owl-main-slider');
+    owl.owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+    });
+    var owl2 = $('#partners');
+    owl2.owlCarousel({
+      items: 4,
+      loop: true,
+      margin: 10,
+      autoplay: true,
       
-    //   animateIn: 'flipInX',
-    //   stagePadding: 30,
-    //   smartSpeed: 450,
-    //   autoplayTimeout: 3000,
-    //   autoplayHoverPause: true,
-    // });
+      animateIn: 'flipInX',
+      stagePadding: 30,
+      smartSpeed: 450,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+    });
 
-    // $(function () {
-    //   owl.trigger('play.owl.autoplay', [1000])
-    //   owl2.trigger('play.owl.autoplay', [1000])
-    // })
+    $(function () {
+      owl.trigger('play.owl.autoplay', [1000])
+      owl2.trigger('play.owl.autoplay', [1000])
+    })
   }
 }
 
